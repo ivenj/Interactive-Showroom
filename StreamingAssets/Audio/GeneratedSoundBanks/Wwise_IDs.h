50a15f0559884ca6e70c8a18e81e751ebaab03a4
Iven Jansen <ivenjansen@gmail.com>
2020-12-01 14:36:36 +0100

    initial commit

---
+++ StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,201 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ALT_THEME = 12725592U;
+        static const AkUniqueID AMBIENCE_SHRINE_BASE_CT = 4268766775U;
+        static const AkUniqueID AMBIENCE_SHRINE_BASE_T = 3425609350U;
+        static const AkUniqueID AMBIENCE_SHRINE_SIDES = 2528006164U;
+        static const AkUniqueID AMBIENCE_SHRINE_TOP = 1606009653U;
+        static const AkUniqueID AR_JOIN = 3854002595U;
+        static const AkUniqueID AR_LEAVE = 2838362144U;
+        static const AkUniqueID AR_PROXIMITY = 1633671670U;
+        static const AkUniqueID AR_TALK = 2573306421U;
+        static const AkUniqueID ASYM = 3283726747U;
+        static const AkUniqueID CTSTART_COM_FOLLOWCOM = 1575473849U;
+        static const AkUniqueID CTSTART_CT_POINT = 628498153U;
+        static const AkUniqueID CTSTART_LOCKNLOAD = 398298476U;
+        static const AkUniqueID CTSTART_MEETME = 3864106304U;
+        static const AkUniqueID CTSTART_MOVEOUT = 3106654164U;
+        static const AkUniqueID CTSTART_STICKTOG = 4120944193U;
+        static const AkUniqueID CTSTART2_COM_GO = 1271649773U;
+        static const AkUniqueID CTSTARTGO = 3410464302U;
+        static const AkUniqueID DUSTNET_TALK = 1880217691U;
+        static const AkUniqueID DUSTNET_TRAILER_1 = 3649420758U;
+        static const AkUniqueID DUSTNET_TRAILER_2 = 3649420757U;
+        static const AkUniqueID EMPTY = 3354297748U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID GAME_WIN_CT = 2573371974U;
+        static const AkUniqueID GAME_WIN_T = 2614555729U;
+        static const AkUniqueID GENERIC_T_POSE = 864092179U;
+        static const AkUniqueID H3CKER = 1436813109U;
+        static const AkUniqueID IJ_DREAMS_OF_POPOL_VUH = 3739873048U;
+        static const AkUniqueID IJ_FERAL = 1583568293U;
+        static const AkUniqueID IJ_LIGHT_OVER_DISTANT_PLANES = 3245339334U;
+        static const AkUniqueID IJ_MATINAL = 2463570649U;
+        static const AkUniqueID IN_M3M0R14M = 2471296950U;
+        static const AkUniqueID LOADSCREEN_CONTINUE = 2387212319U;
+        static const AkUniqueID LOWPASS_OFF = 1455292U;
+        static const AkUniqueID LOWPASS_ON = 1247012854U;
+        static const AkUniqueID MUSIC_GAME = 1072299391U;
+        static const AkUniqueID MUSIC_GAME_AMBIENCE = 382196062U;
+        static const AkUniqueID MUSIC_GAME_AMBIENCE_TEST = 2716529865U;
+        static const AkUniqueID MUSIC_GAME_STOP = 3614514634U;
+        static const AkUniqueID MUSIC_MENU_ESC = 2709723178U;
+        static const AkUniqueID MUSIC_MENU_INTRO = 3721595723U;
+        static const AkUniqueID MUSIC_MENU_RETURN = 3957217861U;
+        static const AkUniqueID MUSIC_MENU_SERVERCONNECT = 2246542544U;
+        static const AkUniqueID MUSIC_MENU_SERVERCONNECT_NEW = 2670999955U;
+        static const AkUniqueID MUSIC_MENU_STOP = 3264661319U;
+        static const AkUniqueID MUSIC_MENU_THEME = 2539831462U;
+        static const AkUniqueID MUSICAL_MOMENT_1 = 3211662488U;
+        static const AkUniqueID MUSICAL_MOMENT_3 = 3211662490U;
+        static const AkUniqueID MUSICAL_MOMENT_6 = 3211662495U;
+        static const AkUniqueID MUSICAL_MOMENT_7 = 3211662494U;
+        static const AkUniqueID MUSICAL_MOMENT_13 = 4064963451U;
+        static const AkUniqueID MUSICAL_MOMENT_15 = 4064963453U;
+        static const AkUniqueID MUSICAL_MOMENT_EGG_MEMORIAL = 1084123527U;
+        static const AkUniqueID MUSICAL_MOMENT_FADEOUT = 2045447803U;
+        static const AkUniqueID MUSICAL_MOMENT_MODECHANGE = 3332756678U;
+        static const AkUniqueID MUSICAL_MOMENT_PORTAL = 2801316717U;
+        static const AkUniqueID MUSICAL_MOMENT_RANDOM = 1306397810U;
+        static const AkUniqueID MUSICAL_MOMENT_STOP = 824029603U;
+        static const AkUniqueID MUTE_ALL = 1739713018U;
+        static const AkUniqueID MUTEALL = 2308878679U;
+        static const AkUniqueID PC_EDITMODE_OFF = 2891709546U;
+        static const AkUniqueID PC_EDITMODE_ON = 3723807564U;
+        static const AkUniqueID PC_JOIN = 3024465861U;
+        static const AkUniqueID PC_LEAVE = 359288750U;
+        static const AkUniqueID PC_PROXIMITY = 1163011608U;
+        static const AkUniqueID PC_TALK = 2544041503U;
+        static const AkUniqueID PHASES_II = 3220605158U;
+        static const AkUniqueID PLAYER_PICKUP_NULLGATE = 4254251482U;
+        static const AkUniqueID PRESSBUTTON = 2561485778U;
+        static const AkUniqueID RE_ENTER = 314049413U;
+        static const AkUniqueID ROUND_WIN_CT = 2656744494U;
+        static const AkUniqueID ROUND_WIN_T = 2175474281U;
+        static const AkUniqueID ROUNDS = 2851108078U;
+        static const AkUniqueID SERVER_COLLAPSE_START = 3495782887U;
+        static const AkUniqueID SERVER_COLLAPSE_STOP = 3408793101U;
+        static const AkUniqueID SHRINES = 476561739U;
+        static const AkUniqueID STINGER_SERVEREXIT = 3500889673U;
+        static const AkUniqueID STOP_ALL = 452547817U;
+        static const AkUniqueID SWIVET_COSME = 1438506203U;
+        static const AkUniqueID SWIVET_GLASS = 919714616U;
+        static const AkUniqueID SWIVET_MOLD = 208642036U;
+        static const AkUniqueID SWIVET_PLATES = 2587332787U;
+        static const AkUniqueID T_M_GAME_MENU_TRANS = 3435687277U;
+        static const AkUniqueID T_M_LSCREEN_MENU_TRANS = 479096259U;
+        static const AkUniqueID T_M_MENU_GAME_TRANS = 3609535243U;
+        static const AkUniqueID TGOED = 10258418U;
+        static const AkUniqueID UI_ESCAPEMENU = 2195135330U;
+        static const AkUniqueID UI_MOUSECLICK = 2131322261U;
+        static const AkUniqueID UI_MOUSECLICK_DISCONNECT = 4170797038U;
+        static const AkUniqueID UI_MOUSECLICK_JOIN = 3543531360U;
+        static const AkUniqueID UI_MOUSEOVER = 3809715957U;
+        static const AkUniqueID UI_TYPING = 1535725427U;
+        static const AkUniqueID UNMUTE_ALL = 4003578643U;
+        static const AkUniqueID UNMUTEALL = 3340787584U;
+        static const AkUniqueID VR_JOIN = 2392416420U;
+        static const AkUniqueID VR_LEAVE = 4172758209U;
+        static const AkUniqueID VR_PROXIMITY = 2433765975U;
+        static const AkUniqueID VR_TALK = 878191054U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_STATE_GROUP
+        {
+            static const AkUniqueID GROUP = 2434766612U;
+
+            namespace STATE
+            {
+                static const AkUniqueID MUSIC_STATE_MENU_CONTINUE = 1454272834U;
+                static const AkUniqueID MUSIC_STATE_MENU_ESC = 2118746164U;
+                static const AkUniqueID MUSIC_STATE_MENU_INTRO = 1293439705U;
+                static const AkUniqueID MUSIC_STATE_MENU_RETURN = 1294170623U;
+                static const AkUniqueID MUSIC_STATE_MENU_SERVERCONNECT = 1790587998U;
+                static const AkUniqueID MUSIC_STATE_MENU_SERVERCONNECT_NEW_1 = 3614420305U;
+                static const AkUniqueID MUSIC_STATE_MENU_SERVERCONNECT_NEW_2 = 3614420306U;
+                static const AkUniqueID MUSIC_STATE_MENU_THEME = 2101066048U;
+            } // namespace STATE
+        } // namespace MUSIC_STATE_GROUP
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BUS_VOLUME_AMBIENCE = 77985649U;
+        static const AkUniqueID BUS_VOLUME_MUSIC = 2849708270U;
+        static const AkUniqueID BUS_VOLUME_SFX = 194530214U;
+        static const AkUniqueID BUS_VOLUME_UI = 2829378447U;
+        static const AkUniqueID C4_SPEED = 531522408U;
+        static const AkUniqueID COLLAPSE_RESET = 2663183476U;
+        static const AkUniqueID MENU_UI_SERVERJOIN_DIST = 3976430696U;
+        static const AkUniqueID MENUTHEME_DISTORTION = 86468267U;
+        static const AkUniqueID MENUTHEME_REVERB = 4099129462U;
+        static const AkUniqueID PERCENT_OPEN = 439026863U;
+        static const AkUniqueID SC_MUSIC = 1834744151U;
+        static const AkUniqueID SC_MUSIC_II = 2780467388U;
+        static const AkUniqueID SC_MUSICAL_EASTEREGG = 1701858376U;
+        static const AkUniqueID SC_SERVERCOLLAPSE = 3232092806U;
+        static const AkUniqueID SC_SHRINES = 3640490394U;
+        static const AkUniqueID SERVER_COLLAPSE_RTPC = 1254454872U;
+        static const AkUniqueID TRIGGER_VELOCITY = 4141066011U;
+        static const AkUniqueID XR_PROXIMITY_RTPC = 4071461597U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID EVERYTHING = 972902454U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID AMBIENT_BACKGROUND_LOOP = 4101057769U;
+        static const AkUniqueID AMBIENT_SERVERCOLLAPSE = 421840853U;
+        static const AkUniqueID AMBIENT_SHRINES = 1922531863U;
+        static const AkUniqueID COMBAT = 2764240573U;
+        static const AkUniqueID INTERACTION = 3900887599U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MENU_MUSIC = 2574280801U;
+        static const AkUniqueID MIXING_BUSES = 3961866393U;
+        static const AkUniqueID MOVEMENT = 2129636626U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID MUSICAL_EASTER_EGG = 2566368846U;
+        static const AkUniqueID MUSICAL_MOMENTS = 3416640774U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID SFX_NULLGATE = 357804863U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID MUSICAL_EASTER_EGG_METER = 3061342550U;
+        static const AkUniqueID ROOMVERB = 1572913279U;
+        static const AkUniqueID SPACEVERB = 1396104734U;
+        static const AkUniqueID SPACEVERB_MUSIC = 1001912054U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
